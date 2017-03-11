a034db42c5dc5225594ddeee2553e1f5e8c7c22d
Jarek on Mac <jaroslaw.wawro@ge.com>
2017-03-11 23:58:54 +0100

    Revert "Material added"
    This reverts commit 3aff86500a5d5102e0bb102e4399dfd41caf382c.

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,9 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
-import { MaterialModule } from '@angular/material';
 
 @NgModule({
-    imports: [BrowserModule,MaterialModule],
+    imports: [BrowserModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
