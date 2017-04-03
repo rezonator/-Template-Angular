62dabd76badbab94abbbb627e255b2edd0d15e81
jaroosh <jarek.wawro@gmail.com>
2017-04-03 17:04:52 +0200

    Custom tabs

--- app/main.ts
+++ app/main.ts
@@ -13,13 +13,17 @@ import {FormsModule} from "@angular/forms";
 import {UserInfoComponent} from "./user/user-info.component";
 import {OrgInfoComponent} from "./org/org-info.component";
 import TaskFilterPipe from "./tasks/tasks-filter.pipe";
-import {TabsModule} from "ngx-tabs";
+/* Tabz */
+import {Tab} from "./shared/tabs/tab.component";
+import {TabHeading} from "./shared/tabs/tab-heading";
+import {Tabset} from "./shared/tabs/tabset.component";
+import {TabTransclude} from "./shared/tabs/tab-transclude";
 
 import "./prod"; // import for side effect.
 
 @NgModule({
-    imports: [BrowserModule, routing,FormsModule, HttpModule,TabsModule],
-    declarations: [AppComponent,TaskItemComponent, TaskListComponent, EnumPipe, GEnumPipe, TaskFilterPipe, BarGraphComponent, UserInfoComponent, OrgInfoComponent],
+    imports: [BrowserModule, routing,FormsModule, HttpModule],
+    declarations: [AppComponent,TaskItemComponent, TaskListComponent, EnumPipe, GEnumPipe, TaskFilterPipe, BarGraphComponent, UserInfoComponent, OrgInfoComponent, Tab, TabHeading, Tabset, TabTransclude],
     bootstrap: [AppComponent]    
 })
 class AppModule { }
